6beb57b6b18711fb502c4ac2a969304ca20df3ac
Fynn <q6nh7hwk4s@privaterelay.appleid.com>
2026-01-13 08:22:10 +0000

    feat: simplify database page by removing header and description

--- app/pages/database.vue
+++ app/pages/database.vue
@@ -1,13 +1,6 @@
 <template>
-  <div class="container mx-auto p-4">
-    <h1 class="text-4xl font-bold mb-4">Icon Database</h1>
-    <p class="text-lg">Browse and manage your icons</p>
-  </div>
+  <Database />
 </template>
 
 <script setup lang="ts">
-// Database page component
 </script>
-
-<style scoped>
-</style>
